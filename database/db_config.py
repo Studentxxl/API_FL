4f18891530669147b8c4b16e857459552a269950
den <galatus@yandex.ru>
2024-08-17 22:26:05 +0300

    параметры дб

--- database/db_config.py
+++ database/db_config.py
@@ -1,5 +1,12 @@
+"""
 dbname = 'postgres'
 user = 'postgres'
 password = 'RTy567'
 host = 'localhost'
 port = '5432'
+"""
+dbname = 'postgres'
+user = 'test'
+password = 'test'
+host = '192.168.88.235'
+port = 25433
